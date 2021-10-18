b7e0343884e8399ad5beef32fd6974b070c22e0e
mereilly <mereilly@loyola.edu>
2021-10-17 22:55:24 -0400

    Update charts.js

--- static/js/charts.js
+++ static/js/charts.js
@@ -5,7 +5,7 @@ function init() {
   var selector = d3.select("#selDataset");
 
   // Use the list of sample names to populate the select options
-  d3.json("samples.json").then((data) => {
+  d3.json("js/samples.json").then((data) => {
     var sampleNames = data.names;
 
     sampleNames.forEach((sample) => {
@@ -58,7 +58,7 @@ function buildMetadata(sample) {
 // 1. Create the buildCharts function.
 function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
-  d3.json("samples.json").then((data) => {
+  d3.json("js/samples.json").then((data) => {
 
     /////////////////// Deliverable 1 ////////////////////////
 
